9987a7128f10f54c9bd961231b6998b125e51dd2
yongsk0066 <yongsk0066@naver.com>
2024-05-17 22:26:48 -0700

    feat: 인자 받도록

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    target_dir: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("Target directory: {}", args.target_dir);
 }
